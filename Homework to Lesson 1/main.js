714a40102ebc16d1cbd31bf67d9627d57eb85466
Waldemar91 <volodymyr.shenbor@gmail.com>
2023-08-16 19:52:54 +0300

    finished 1 homework tasks

---
+++ Homework to Lesson 1/main.js
@@ -0,0 +1,46 @@
+let a = 'hello';
+let b= 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10
+let g = -999
+let h = 123
+let i = 3.14
+let j = 2.7
+let k = 16
+let l = true
+let m = false
+
+console.log(a);
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(k)
+console.log(l)
+console.log(m)
+
+
+//2 завдання
+
+let firstName = "Volodymyr ";
+let middleName = "Orestovych ";
+let lastName = 'Shenbor';
+let person = firstName + middleName +lastName;
+console.log(person)
+
+
+//3 завдання
+
+let s1 = 100;
+let s2 = '100';
+let s3 = true;
+console.log(typeof s1);
+console.log(typeof s2);
+console.log(typeof s3);
