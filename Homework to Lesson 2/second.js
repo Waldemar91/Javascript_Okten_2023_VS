f8c74031566ec6878f7688537e983dabb3c8cf81
Waldemar91 <volodymyr.shenbor@gmail.com>
2023-08-17 17:12:45 +0300

    start resolving HW2

---
+++ Homework to Lesson 2/second.js
@@ -0,0 +1,50 @@
+// task 2.1
+let arr;
+arr = [11, 08, 24, 1991, 16, 30, -100, 'car', 'box', 50];
+console.log(arr);
+
+//task 2.2
+let book1'={
+    title: 'Okten',
+    pageCount:100,
+    genre:'fantasy'
+};
+let book2={
+    title:'OktenKyiv',
+    pageCount:50,
+    genre:'fantasy'
+};
+let book3={
+    title:'OktenWarsaw',
+    pageCount:70,
+    genre:'fantasy'
+};
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
+
+// task 2.3
+
+let booking = {
+    title: 'Titanic',
+    pageCount:495,
+    genre: 'romantic',
+    authors: ['name','age']
+}
+
+// task 2.4
+let using = [
+    {name:'Igor',username:'A',password: 123},
+    {name:'Maksym',username:'B',password: 456},
+    {name:'Viktor',username:'C',password: 789},
+    {name:'Volodymyr',username:'D',password:589},
+    {name:'Anna',username:'E',password:254},
+    {name:'Maria',username:'F',password:356},
+    {name:'Julia',username:'G',password:695},
+    {name:'Oksana',username:'H',password:451},
+    {name:'Svitlana',username:'I',password:378},
+    {name:'Yana',username:'J',password:697}
+
+]
+console.log{using.password}
