0fc2081592da20b1fe3609e42f83db72ff4ac970
Waldemar91 <volodymyr.shenbor@gmail.com>
2023-08-18 09:27:30 +0300

    if tasks - start

--- Homework to Lesson 2/second.js
+++ Homework to Lesson 2/second.js
@@ -4,11 +4,11 @@ arr = [11, 08, 24, 1991, 16, 30, -100, 'car', 'box', 50];
 console.log(arr);
 
 //task 2.2
-let book1'={
-    title: 'Okten',
-    pageCount:100,
-    genre:'fantasy'
-};
+// let book1'={
+//     title: 'Okten',
+//     pageCount:100,
+//     genre:'fantasy'
+// };
 let book2={
     title:'OktenKyiv',
     pageCount:50,
@@ -47,4 +47,35 @@ let using = [
     {name:'Yana',username:'J',password:697}
 
 ]
-console.log{using.password}
+console.log(using[0].password);
+console.log(using[1].password);
+console.log(using[2].password);
+console.log(using[3].password);
+console.log(using[4].password);
+console.log(using[5].password);
+console.log(using[6].password);
+console.log(using[7].password);
+console.log(using[8].password);
+console.log(using[9].password);
+
+//Логічні розгалуження
+//1.
+
+let x = prompt('enter your number');
+if (x!=0) {console.log('True')}
+else {(console.log('False')};
+
+//2.
+let time = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59}
+if (time = 0-15) {console.log('firstQuarter')}
+else if (time = 16-30) {console.log('secondQuarter')}
+else if (time = 31-45) {console.log('thirdQuarter')}
+else if (time = 46-59) {console.log('fourthQuarter')}
+//3.
+let day = {'як записати діапазон від до '}
+
+
+//4.
+
+
+//5.
