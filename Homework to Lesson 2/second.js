6af3c2fc1be2cf757e42ff4bcbcf832375b81880
Waldemar91 <volodymyr.shenbor@gmail.com>
2023-08-21 23:59:25 +0300

    finish HW2

--- Homework to Lesson 2/second.js
+++ Homework to Lesson 2/second.js
@@ -1,26 +1,24 @@
-// task 2.1
+// task 2.1 Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 let arr;
 arr = [11, 08, 24, 1991, 16, 30, -100, 'car', 'box', 50];
-console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
 
 //task 2.2
-// let book1'={
-//     title: 'Okten',
-//     pageCount:100,
-//     genre:'fantasy'
-// };
-let book2={
-    title:'OktenKyiv',
-    pageCount:50,
-    genre:'fantasy'
-};
+
 let book3={
     title:'OktenWarsaw',
     pageCount:70,
     genre:'fantasy'
 };
-console.log(book1);
-console.log(book2);
 console.log(book3);
 
 
@@ -30,8 +28,12 @@ let booking = {
     title: 'Titanic',
     pageCount:495,
     genre: 'romantic',
-    authors: ['name','age']
-}
+    authors: [
+        {name:'Antonio', age: 13}
+        {name:'Antoni', age: 45},
+    ]
+};
+console.log(booking);
 
 // task 2.4
 let using = [
@@ -61,51 +63,52 @@ console.log(using[9].password);
 //Логічні розгалуження
 //1.
 
-let x = prompt('enter your number');
+let x =5;
 if (x!=0) {console.log('True')}
 else {(console.log('False')};
 
 //2.- Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
-let time = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59}
+let time = 18;
 if (time>=0 & time<=15) {console.log('firstQuarter');}
-else if (time>=16 & time<=30) {console.log('secondQuarter');}
-else if (time>=31 & time<=45) {console.log('thirdQuarter');}
-else if (time>=46 & time<=59) {console.log('fourthQuarter');}
+else if (time>=16 && time<=30) {console.log('secondQuarter');}
+else if (time>=31 && time<=45) {console.log('thirdQuarter');}
+else if (time>=46 && time<=59) {console.log('fourthQuarter');}
 
 //3.- У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
-let day = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
-if (day>=1 & day<=10) {console.log('1 декада місяця');}
-else if (day>=11 & day<=20) {console.log('2 декада місяця');}
-else (day>=21 & day<=31) {console.log('3 декада місяця'),}
+let day = 30;
+if (day>0 && day<=10) {console.log('1 декада місяця');}
+else if (day>10 && day<=20) {console.log('2 декада місяця');}
+else (day>20 && day<=31) {console.log('3 декада місяця'),}
+else { console.log('Wrong day')}
 
 
 //4.Timetable - switch- Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
-let numberday = prompt('enter number day of the week');
-switch (numberday) {
-    case '1'://число вводити без одинарних лапок?
+let day2=6;
+switch (day2) {
+    case 1:
         console.log('Monday!');
         break;
-    case '2':
+    case 2:
         console.log('Tuesday');
         break;
-    case '3':
+    case 3:
         console.log('Wednesday');
         break;
-        case '4':
+        case 4:
         console.log('Thursday');
         break;
-        case '5':
+        case 5:
         console.log('Friday');
         break;
-        case '6':
+        case 6:
         console.log('Saturday');
         break;
-        case '7':
+        case 7:
         console.log('Sunday');
         break;
     default:
-        console.log('????');
+        console.log('Error');
 }
 
 //5. Користувач вводить або має два числа.
@@ -115,12 +118,42 @@ let y=4;
 let z = 6;
 if (y>z) {console.log('y');}
 else if (y<z) {console.log('z');}
-else (y=z) {console.log('y=z')}
-
+else if (y===z) {console.log('equal');}
+else { console.error('Error')}
 
 //6. Є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
-
+let x;
+x = x || 'default';
+x = x ?? 'default';
+console.log(x)
 
 
 //7.- з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+]
+if (coursesAndDurationArray[0].monthDuration>5) {
+    console.log ('Super!!!');
+}
+if (coursesAndDurationArray[1].monthDuration>5) {
+    console.log ('Super!!!');
+}
+if (coursesAndDurationArray[2].monthDuration>5) {
+    console.log ('Super!!!');
+}
+if (coursesAndDurationArray[3].monthDuration>5) {
+    console.log ('Super!!!');
+}
+if (coursesAndDurationArray[4].monthDuration>5) {
+    console.log ('Super!!!');
+}
+if (coursesAndDurationArray[5].monthDuration>5) {
+    console.log ('Super!!!');
+}
